7ab7ec794ce1b58bbd4f44659af649e3d1d96717
fatima <fatimahassanhassab8@gmail.com>
2024-07-08 17:22:22 -0700

    change to priorityx

--- src/App.test.js
+++ src/App.test.js
@@ -22,7 +22,7 @@ test("Renders Table Header - Task Column", () => {
 
 test("Renders Table Header - Priority Column", () => {
   render(<App />);
-  const linkElement = screen.getByText("Priority");
+  const linkElement = screen.getByText("Priorityx");
   expect(linkElement).toBeInTheDocument();
 });
 
